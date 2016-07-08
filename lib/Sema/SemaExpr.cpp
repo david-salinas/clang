c62026b5471716030b37303552c5e025fe7b8abe
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2016-07-08 14:10:31 -0400

    [OpenCL] Use opaque struct for sampler.

--- lib/Sema/SemaExpr.cpp
+++ lib/Sema/SemaExpr.cpp
@@ -7600,6 +7600,11 @@ Sema::CheckAssignmentConstraints(QualType LHSType, ExprResult &RHS,
     }
   }
 
+  if (LHSType->isSamplerT() && RHSType->isIntegerType()) {
+    Kind = CK_IntToOCLSampler;
+    return Compatible;
+  }
+
   return Incompatible;
 }
 
